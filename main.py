0bb549242816f6cab6efc15f5c9d6c5263ab2962
Mohammad Rad <argo.mohrad@gmail.com>
2026-06-12 00:29:13 -0700

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Mount the static dir
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root():
+    return FileResponse("templates/index.html")
